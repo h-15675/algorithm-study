22c345ccea5d68fab3e53903807b273f24d6fecc
jhong2282 <jhong2282@naver.com>
2021-07-26 14:28:56 +0900

    algorithm-study

---
+++ 9498.py
@@ -0,0 +1,11 @@
+A=int(input())
+if 100>= A >= 90 :
+      print("A")
+elif 89>= A >= 80 :
+    print("B")
+elif 79>= A >= 70 :
+    print("C")
+elif 69>= A >= 60 :
+    print("D")
+else :
+    print("F")
