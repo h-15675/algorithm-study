337cb89831d2d6b687084d61b25d4eb8337f14df
Jae hong Park <43770022+jhong2282@users.noreply.github.com>
2021-08-06 13:49:16 +0900

    Update 1110.py

--- 1110.py
+++ 1110.py
@@ -1,21 +1,11 @@
-A = int(input())
-a = A // 10
-b = A-a*10 
-c = a +b
-if c < 10 :
-    d = c
-if c >= 10 :
-    d= c- 10
-
-A = int(input())
-a = A // 10
-b = A-a*10 
-a = A // 10
-b = A-a*10 
-c = a +b
-if c < 10 :
-    d = c
-if c >= 10 :
-    d= c- 10
-e = b+e
-N = 0
+A=B = int(input())
+N= 0
+while True :
+    ten = A // 10
+    one = A%10
+    total = ten +one
+    N = N+1
+    A =int(str(A%10)+ str(total%10))
+    if (B == A) :
+        break
+print(N)
