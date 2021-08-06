9cf07a73a8f516b4004cfebd7ebe5564a6113db2
jhong2282 <jhong2282@naver.com>
2021-08-06 13:20:51 +0900

    algorithm-study

---
+++ 1110.py
@@ -0,0 +1,21 @@
+A = int(input())
+a = A // 10
+b = A-a*10 
+c = a +b
+if c < 10 :
+    d = c
+if c >= 10 :
+    d= c- 10
+
+A = int(input())
+a = A // 10
+b = A-a*10 
+a = A // 10
+b = A-a*10 
+c = a +b
+if c < 10 :
+    d = c
+if c >= 10 :
+    d= c- 10
+e = b+e
+N = 0
