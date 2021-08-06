ae969889fe6c0cee8904a177032f6d4b887264f1
jhong2282 <jhong2282@naver.com>
2021-08-07 02:33:15 +0900

    algorithm-study

--- 1110-1.py
+++ 1110-1.py
@@ -1,11 +1,11 @@
-A=B= int(input())
+A=B = int(input())
 N= 0
 while True :
     ten = A // 10
     one = A%10
     total = ten +one
     N = N+1
-    A =int(A%10)*10+ int(total%10)
+    A =int(str(A%10)+ str(total%10))
     if (B == A) :
         break
 print(N)
