f08dc8408c456be91e8dc672052a4aeff7cc5c8d
jhong2282 <jhong2282@naver.com>
2021-08-11 16:48:41 +0900

    algorithm-study

---
+++ 4344-2.py
@@ -0,0 +1,8 @@
+for i in range(int(input())):
+    A= list(map(int,input().split()))
+    avg = (sum(A)-A[0])/A[0]
+    stu = 0
+    for j in A[1:]:
+        if j > avg:
+            stu +=1
+    print(f'{(stu/A[0]*100):.3f}%')
