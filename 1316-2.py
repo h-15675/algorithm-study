7628b155a3fbb648937f81e55de2cd8a6b254808
jhong2282 <jhong2282@naver.com>
2021-09-05 22:05:06 +0900

    algorithm-study

---
+++ 1316-2.py
@@ -0,0 +1,10 @@
+N= int(input())
+for i in range(N):
+    S=input()
+    for j in range(len(S)-1):
+        if S[j] != S[j+1]:
+            pass
+        elif S[j] in S[j+1:]:
+            N -=1
+            break
+print(N)
