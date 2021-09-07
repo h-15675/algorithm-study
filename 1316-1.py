7455dfd602b2ce0ec7f7bfb5f4e56d1214b6984c
jhong2282 <jhong2282@naver.com>
2021-09-07 14:37:41 +0900

    algorithm-study

--- 1316-1.py
+++ 1316-1.py
@@ -2,8 +2,7 @@ N= int(input())
 for i in range(N):
     S=input()
     for j in range(len(S)-1):
-        if S[j] != S[j+1]:
-            if S[j] in S[j+1:]:
-                 N -=1
-                 break
+        if S[j] != S[j+1] and S[j] in S[j+1:]:
+            N -=1
+            break
 print(N)
