88c05bdc3fc9635a4e868b6d44a40735a03dc9b1
jhong2282 <jhong2282@naver.com>
2021-09-19 23:55:36 +0900

    algorithm-study

--- R18.R
+++ R18.R
@@ -1,28 +1,19 @@
-df <- data.frame(sex = c("M","F", NA, "M", "F"), 
-        score = c(5, 4, 3, 4, NA)) #반드시 따옴표 없이 대문자로 NA
-is.na(df)
-table(is.na(df))
-table(is.na(df$sex))
-table(is.na(df$score))
-library(dplyr)
-df %>% filter(is.na(score))
-df %>% filter(!is.na(score)) #score 결측치 제거
-df_nomiss <- df %>% filter(!is.na(score))
-mean(df_nomiss$score)
-sum(df_nomiss$score)
-df_nomiss <- df %>% filter(!is.na(score) & ! is.na(sex)) #score, sex 결측치 제거 (1)
-df_nomiss2 <- na.omit(df) #하나라도 결측치가 있다면 지워라 (2)
-mean(df$score, na.rm = T) #결측치를 제외하고 평균 산출
-sum(df$score, na.rm = T) # 결측치를 제외하고 합계 산출
-exam <- read.csv("csv_exam.csv")
-exam[c(3, 8, 15), "math"] <- NA #3,8,15행의 math에 NA를 할당
-exam %>% summarise(mean_math = mean(math)) #math 평균산출
-exam %>% summarise(mean_math = mean(math, na.rm = T)) #math 결측치를 제외하고 평균 산출
-exam %>% summarise(mean_math = mean(math, na.rm = T),
-                   sum_math = sum(math, na.rm = T),
-                   median_math = median(math, na.rm = T)) 
-mean(exam$math, na.rm = T) #결측치 제외하고 math 평균 산출
-exam$math <- ifelse(is.na(exam$math), 55, exam$math) #결측치(NA)이면 55를 넣고 아닌 것은 원래 있는 값을 그대로 넣겠다. 
-table(is.na(exam$math))
-exam
-mean(exam$math)
+#실습 2
+mpg <- as.data.frame(ggplot2::mpg)
+mpg[c(10,14,58,93), "drv"] <- "k"
+mpg[c(29,43,129,203), "ctv"] <- c(3,4,39,42)
+table(mpg$drv)
+
+mpg$drv <- ifelse(mpg$drv %in% c("4","f","r"), mpg$drv, NA)
+table(mpg$drv)
+
+boxplot(mpg$cty)$stats
+mpg$cty <- ifelse(mpg$cty < 9 | mpg$cty > 26, NA, mpg$cty)
+boxplot(mpg$cty)
+mpg %>% filter(!is.na(drv) & !is.na(cty)) %>% group_by(drv) %>% summarise(mean_hwy = mean(cty))
+
+library(ggplot2)
+ggplot(data = mpg, aes(x = displ, y = hwy))
+ggplot(data = mpg, aes(x = displ, y = hwy)) + geom_point()
+ggplot(data = mpg, aes(x = displ, y = hwy)) + geom_point() + xlim(3, 6)
+ggplot(data = mpg, aes(x = displ, y = hwy)) + geom_point() + xlim(3, 6) + ylim(10, 30)
