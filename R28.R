6edd7b9a54fb412ac557faeceee700d5775ea5c3
jhong2282 <jhong2282@naver.com>
2021-10-03 21:39:45 +0900

    algorithm-study

---
+++ R28.R
@@ -0,0 +1,8 @@
+#성별 직업 빈도 
+job_male <-welfare %>%filter(!is.na(job) &sex =="male") %>%group_by(job) %>%summarise(n =n()) %>%arrange(desc(n)) %>%head(10)
+job_female <-welfare %>%filter(!is.na(job) &sex =="female") %>%group_by(job) %>%summarise(n =n()) %>%arrange(desc(n)) %>%head(10)
+job_female
+
+ggplot(data =job_male, aes(x =reorder(job, n), y =n)) +geom_col() +coord_flip()
+
+ggplot(data =job_female, aes(x=reorder(job, n), y =n)) + geom_col() +coord_flip()
