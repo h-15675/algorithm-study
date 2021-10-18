1c995ee997c67ae58ab9d8bef4f841f70070f086
jhong2282 <jhong2282@naver.com>
2021-10-18 11:22:39 +0900

    algorithm-study

---
+++ R38.R
@@ -0,0 +1,12 @@
+install.packages("plotly") 
+library(plotly)
+library(ggplot2) 
+p <- ggplot(data = mpg, aes(x = displ, y = hwy, col = drv)) + geom_point()
+ggplotly(p)
+p <- ggplot(data = diamonds, aes(x = cut, fill = clarity)) + geom_bar(position = "dodge") 
+ggplotly(p)
+ggplotly(ggplot(data = diamonds, aes(x = cut, fill = clarity)) + geom_bar(position = "dodge"))
+install.packages("dygraphs") 
+library(dygraphs)
+economics <- ggplot2::economics 
+head(economics)
